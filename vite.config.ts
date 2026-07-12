ef41eea8a3acdae6990d7aa5a8d1ddbd86f13a93
saucedontsauce <98565306+saucedontsauce@users.noreply.github.com>
2026-07-12 02:44:46 +0100

    feat: initial setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,17 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel';
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
 import * as path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
+  base: "/torn-stuff/",
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
   resolve: {
     alias: {
-    '@': path.resolve(import.meta.dirname, './src'),
-    '@shared': path.resolve(import.meta.dirname, './src/shared'),
-    '@pages': path.resolve(import.meta.dirname, './src/pages/index.ts'),
+      "@": path.resolve(import.meta.dirname, "./src"),
+      "@shared": path.resolve(import.meta.dirname, "./src/shared"),
+      "@pages": path.resolve(import.meta.dirname, "./src/pages/index.ts"),
+    },
   },
-  },
-  
-})
+});
