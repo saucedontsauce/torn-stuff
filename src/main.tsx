17671718fbd4d58dc12c05db366372148923748b
saucedontsauce <98565306+saucedontsauce@users.noreply.github.com>
2026-07-12 02:56:04 +0100

    feat: hashrouter conversion

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./assets/index.css";
 import "bootswatch/dist/morph/bootstrap.min.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
